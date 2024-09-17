37f53614a853a87e4373ca058ce4e484cde0ca54
AnuragAnand14 <anurag.anand@blenheimchalcot.com>
2024-09-17 21:50:10 +0530

    Update consolidated.py

--- consolidated.py
+++ consolidated.py
@@ -45,6 +45,8 @@ os.environ["OPENAI_API_KEY"] = secrets["openai_api_key"]
 def load_css(file_path):
     with open(file_path) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+      
+load_css('styles2.css')
 
 def is_valid_uuid(val):
     try:
