5f65a69d95a86d69af25ec80ea10f52ecae1bcf4
AnuragAnand14 <anurag.anand@blenheimchalcot.com>
2024-10-01 16:30:20 +0530

    Update consolidated3.py

--- consolidated3.py
+++ consolidated3.py
@@ -597,13 +597,9 @@ def main():
             st.info("Sample ID: 123e4567-e89b-12d3-a456-426614174000")
 
         # Add a button to download a sample zip file from GitHub
-        github_zip_url = "https://github.com/AnuragAnand14/ModulrCRM-KYC/tree/anu111/Driving%20License"
-        if st.download_button(
-            label="Download Sample Documents",
-            data=fetch_github_zip(github_zip_url),
-            file_name="sample_documents.zip",
-            mime="application/zip"
-        ):
+            file_id = "1p2LpltzuXM86XbPRHGmwQUXisZZ9Xits"
+            google_drive_link = f"https://drive.google.com/uc?export=download&id={file_id}"
+            st.markdown(f"[Download Sample Documents]({google_drive_link})")
             st.success("Sample documents downloaded successfully!")
     with col3:
         st.markdown("")
